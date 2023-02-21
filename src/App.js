f78c77f6a7ec2d2c45f53b405c729ea3e5a54e28
Rohit <rohitmore386@gmail.com>
2023-02-21 17:19:41 +0530

    nav route location

--- src/App.js
+++ src/App.js
@@ -8,30 +8,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    function handleScroll() {
-      setScrollPosition(window.pageYOffset);
-      if (scrollPosition > 650) {
-        return 'scrolled';
-      }
-      return '';
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [scrollPosition]);
-
   return (
     <>
-      {scrollPosition > 650 ? (
-        <Navbar />
-      ) : null}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about/governor" element={<About />} />
